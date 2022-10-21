41ad7cfeaa2d4c9a3724773206cbd4350bf4da85
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-21 23:14:26 +0530

    Add files via upload
    added uc2

---
+++ LastNameRegixx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LastNameRegix
+{
+   
+    
+    internal class LastNameRegixx
+    {
+        
+        public static string LastName = "^[A-Z]{1}[a-z]{2}$";
+        public bool validate_lastName(string lastname)
+        {
+            return Regex.IsMatch(LastName,lastname);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ValidateUsingREGEX
+namespace LastNameRegix
 {
+
     internal class Program
+
     {
         public static void Main(string[] args)
         {
-            Uc1_Regexfirstname pattern = new Uc1_Regexfirstname();
-            Console.WriteLine(pattern.validate_FirstName("Ani"));
+            LastNameRegixx pattern = new LastNameRegixx();
+            Console.WriteLine(pattern.validate_lastName("Kat"));
+           
         }
     }
 }
