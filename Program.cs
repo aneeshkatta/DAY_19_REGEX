2da0dd72bd10c4f24aad7be263a806c1ac59a31c
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-21 23:12:52 +0530

    Add files via upload
    added uc1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateUsingREGEX
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Uc1_Regexfirstname pattern = new Uc1_Regexfirstname();
+            Console.WriteLine(pattern.validate_FirstName("Ani"));
+        }
+    }
+}
---
+++ Uc1_Regexfirstname.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ValidateUsingREGEX
+{
+    internal class Uc1_Regexfirstname
+    {
+        public static string FirstName = "^[A-Z]{1}[a-z]{2}$";
+        public bool validate_FirstName(string firstname)
+        {
+            return Regex.IsMatch(firstname, FirstName);
+        }
+    }
+}
